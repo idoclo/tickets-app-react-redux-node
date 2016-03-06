04abb67741d3f50240642e76c06865de315206e6
Huanhuan Huang <huanhuan.huang@betterbills.com>
2016-03-06 22:25:08 +1100

    try to handle the action

--- client/components/deviceType.jsx
+++ client/components/deviceType.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Button } from 'react-bootstrap'
 import { ButtonGroup } from 'react-bootstrap'
+import { setDeviceType } from '../actions'
+import { connect } from 'react-redux'
 
-const DeviceType = () => (
-  <div>
-    <h1>What is your device type?</h1>
-    <ButtonGroup>
-      <Button bsSize="sm" block>IPhone</Button>
-      <Button bsSize="sm" block>MacBook</Button>
-    </ButtonGroup>
-  </div>
-)
+class DeviceType extends Component {
+  handleClick() {
+    console.log(this.pros);
+    this.pros.dispatch(setDeviceType(e.target.text))
+  }
+  render() {
+    return (
+      <div>
+        <h1>What is your device type?</h1>
+        <ButtonGroup>
+          <Button bsSize="sm" block onclick={this.handleClick.bind(this)}>IPhone</Button>
+          <Button bsSize="sm" block onclick={this.handleClick.bind(this)}>MacBook</Button>
+        </ButtonGroup>
+      </div>
+    )
+  }
+}
 
 export default DeviceType
 
