d56a831e5957619dc8e03e8fa77eae60e6775ba6
Huanhuan Huang <huanhuan.huang@betterbills.com>
2016-03-07 14:44:26 +1100

    added middleware logger

--- client/index.jsx
+++ client/index.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import { render } from 'react-dom'
+import { Router, Route, useRouterHistory, IndexRoute } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
 import { Provider } from 'react-redux'
-import { Router, Route, browserHistory } from 'react-router'
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import { createHashHistory } from 'history'
+import { render } from 'react-dom'
+import React from 'react'
 import App from './components/app'
-import store from './store'
+import configure from './store'
 
-const history = syncHistoryWithStore(browserHistory, store);
+const store = configure();
+const cleanHashHistory = useRouterHistory(createHashHistory)({ queryKey: false });
+const history = syncHistoryWithStore(cleanHashHistory, store);
 
 let reactElement = document.getElementById('react');
 render(
   <Provider store={store}>
     <Router history={history}>
-      <Route path="/" component={App}/>
+      <Route path="/">
+        <IndexRoute component={App}/>
+      </Route>
     </Router>
-  </Provider>
-  ,
+  </Provider>,
   reactElement
 );
--- client/store/index.js
+++ client/store/index.js
@@ -1,4 +1,27 @@
-import {createStore} from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { browserHistory } from 'react-router'
+
+import { loggerMiddleware, thunkMiddleware } from '../middleware'
 import reducers from '../reducers'
 
-export default createStore(reducers)
+export default (initialState) => {
+  const create = window.devToolsExtension
+    ? window.devToolsExtension()(createStore)
+    : createStore;
+
+  const createStoreWithMiddleware = applyMiddleware(
+    loggerMiddleware,
+    thunkMiddleware
+  )(create);
+
+  const store = createStoreWithMiddleware(reducers, initialState);
+
+  if (module.hot) {
+    module.hot.accept('../reducers', () => {
+      const nextReducer = require('../reducers');
+      store.replaceReducer(nextReducer)
+    })
+  }
+
+  return store
+}
